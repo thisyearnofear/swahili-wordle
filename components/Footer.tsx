aadbf484805de5babfb3fc72775ba509f14b0663
iswilljr <iswilljr@gmail.com>
2023-01-10 12:18:50 -0500

    chore: migrate Link's to Next 13

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Footer() {
+export function Footer() {
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer__top">
-          <Link href="/">
-            <a className="footer__logo">
-              <Image width={227.67} height={22} src="/wordle/logo.svg" alt="Wordle Game - Play Unlimited" />
-            </a>
+          <Link href="/" className="footer__logo">
+            <Image width={227.67} height={22} src="/wordle/logo.svg" alt="Wordle Game - Play Unlimited" priority />
           </Link>
         </div>
         <div className="footer__bottom">
--- pages/404.tsx
+++ pages/404.tsx
@@ -5,8 +5,8 @@ export default function NotFound() {
     <div className="b404">
       <div className="b404__title">404</div>
       <div className="b404__text">Oops! Page not found</div>
-      <Link href="/">
-        <a className="b404__btn">Mainpage</a>
+      <Link href="/" className="b404__btn">
+        Mainpage
       </Link>
     </div>
   );
