7c05bcbf9557cd34ccb105684171f6cb169cad4b
iswilljr <iswilljr@gmail.com>
2023-01-10 14:05:35 -0500

    feat: add aria labels to header buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,18 +22,20 @@ export function Header({ colorScheme }: { colorScheme: "light" | "dark" }) {
         </Link>
       </div>
       <div className="cont flex">
-        <a className="lang">EN</a>
-        <button type="button" className="generator" style={{ display: "block" }}>
+        <a className="lang" href="#lang">
+          EN
+        </a>
+        <button type="button" className="generator" style={{ display: "block" }} aria-label="Create a Game">
           <PlusCircleIcon width="20" height="20" />
         </button>
         <div className="buttons flex">
-          <button type="button" className="button">
+          <button type="button" className="button" aria-label="Charts">
             <ChartBarIcon width="20" height="20" />
           </button>
-          <button type="button" className="button">
+          <button type="button" className="button" aria-label="Settings">
             <CogIcon width="22" height="22" />
           </button>
-          <button type="button" className="button" onClick={() => toggleColorTheme()}>
+          <button type="button" className="button" onClick={() => toggleColorTheme()} aria-label="Chage theme">
             <SparklesIcon width="22" height="22" />
           </button>
         </div>
