f29da099e296d1b62fef893297aaf9ae94b801e6
iswilljr <iswilljr@gmail.com>
2023-01-10 13:44:30 -0500

    chore: avoid importing words.json

--- utils/data.ts
+++ utils/data.ts
@@ -1,4 +1,3 @@
-import words from "./words.json";
 import { encode, decode } from "js-base64";
 
 export type Class = "letter-correct" | "letter-elsewhere" | "letter-absent";
@@ -10,27 +9,27 @@ export interface Word {
 
 export type Letters = Record<string, number>;
 
-function getLetters(letters: string[]): Letters {
+function getLetters(letters: string[], words: string[]): Letters {
   const ltrs: Letters = {};
   letters.forEach((ltr) => (ltrs[ltr] = (ltrs[ltr] || 0) + 1));
   return ltrs;
 }
 
-export function getRandomWord() {
+export function getRandomWord(words: string[]) {
   return encode(words[Math.floor(Math.random() * words.length)]);
 }
 
-export function existsWord(word: string) {
+export function existsWord(word: string, words: string[]) {
   return words.includes(word.toLowerCase());
 }
 
-export function getKeys(word: string, letters: string[]): Word[] {
+export function getKeys(word: string, letters: string[], words: string[]): Word[] {
   const _word = word.toUpperCase();
   const _letters = letters.map((str) => str.toUpperCase());
 
   if (_word === _letters.join("")) return _letters.map((letter) => ({ key: letter, class: "letter-correct" }));
 
-  const lettersCounter = getLetters(_word.split(""));
+  const lettersCounter = getLetters(_word.split(""), words);
   const result: Word[] = [];
 
   _letters.forEach((letter, i) => {
@@ -54,13 +53,17 @@ export function getKeys(word: string, letters: string[]): Word[] {
   return result;
 }
 
-export const checkWord = (word: string, check: string): { exists: false } | { exists: true; keys: Word[] } => {
+export const checkWord = (
+  word: string,
+  check: string,
+  words: string[]
+): { exists: false } | { exists: true; keys: Word[] } => {
   const _word = decode(word);
-  const isValidQueryWord = existsWord(_word.toLowerCase());
-  const isValidQueryCheck = existsWord(check.toLowerCase());
+  const isValidQueryWord = existsWord(_word.toLowerCase(), words);
+  const isValidQueryCheck = existsWord(check.toLowerCase(), words);
   if (!isValidQueryCheck || !isValidQueryWord) return { exists: false };
 
-  const keys = getKeys(_word.toLowerCase(), check.toLowerCase().split(""));
+  const keys = getKeys(_word.toLowerCase(), check.toLowerCase().split(""), words);
   return { exists: true, keys };
 };
 
