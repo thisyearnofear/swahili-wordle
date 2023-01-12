a0f76f0611add29e9762e019b10bdea2f892c91b
iswilljr <iswilljr@gmail.com>
2023-01-12 18:02:07 -0500

    chore: split store utils

--- store/appSlice.ts
+++ store/appSlice.ts
@@ -1,83 +1,31 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { encode } from "js-base64";
-import { DEFAULT_NUMBER_OF_LETTERS } from "utils/numbers-of-letters";
 import { resetGame } from "utils/reset-game";
+import { createInitialState, createSetState, restartGameAction, RootState } from "utils/store";
 
-interface AppState {
-  isSettingsActive: boolean;
-  numberOfLetters: number;
-  backspace: boolean;
-  currentRow: number;
-  enter: boolean;
-  gameIs: "playing" | "won" | "lost";
-  keys: Record<number, string[]>;
-  modal: { content: string; isOpen: boolean; showButton: boolean };
-  word: string;
+interface StartGamePayload {
   words: string[];
+  numberOfLetters: number;
 }
-
-const initialState: AppState = {
-  isSettingsActive: false,
-  numberOfLetters: DEFAULT_NUMBER_OF_LETTERS,
-  backspace: false,
-  currentRow: 0,
-  enter: false,
-  gameIs: "playing",
-  keys: { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [] },
-  modal: { isOpen: false, content: "", showButton: false },
-  word: "",
-  words: [],
-};
-
-const createSetState = <T extends keyof AppState>(property: T) => {
-  return (state: AppState, action: PayloadAction<AppState[T]>) => {
-    state[property] = action.payload;
-  };
-};
-const getRandomWord = (words: string[]) => encode(words[Math.floor(Math.random() * words.length)]);
-
-const getWords = (words: string[], length: number) => {
-  return words.filter((word): word is string => typeof word === "string" && word.length === length);
-};
-
-const restartGameAction = (state: AppState, words?: string[], numberOfLetters?: number): AppState => {
-  const wordsWithNumberOfLetters = getWords(words ?? state.words, numberOfLetters ?? state.numberOfLetters);
-
-  return {
-    ...initialState,
-    numberOfLetters: numberOfLetters ?? state.numberOfLetters,
-    words: words ?? state.words,
-    word: getRandomWord(wordsWithNumberOfLetters),
-    gameIs: "playing",
-  };
-};
 
 export const appSlice = createSlice({
   name: "app",
-  initialState,
+  initialState: createInitialState(),
   reducers: {
-    startGame: (
-      state,
-      { payload: { numberOfLetters, words } }: PayloadAction<{ words: string[]; numberOfLetters: number }>
-    ) => {
+    startGame: (state, { payload: { numberOfLetters, words } }: PayloadAction<StartGamePayload>) => {
       return restartGameAction(state, words, numberOfLetters);
     },
+    setNumberOfLetter: (state, action: PayloadAction<number>) => {
+      resetGame();
+      return restartGameAction(state, state.words, action.payload);
+    },
     restartGame: (state) => restartGameAction(state),
-    setCurrentKeys: (state, action: PayloadAction<Record<number, string[]>>) => {
-      state.keys = { ...state.keys, ...action.payload };
-    },
-    setModal: (state, action: PayloadAction<Partial<AppState["modal"]>>) => {
-      state.modal = { ...state.modal, ...action.payload };
-    },
+    setCurrentKeys: createSetState("keys", true),
+    setModal: createSetState("modal", true),
     setBackspace: createSetState("backspace"),
     setCurrentRow: createSetState("currentRow"),
     setEnter: createSetState("enter"),
     setGameIs: createSetState("gameIs"),
     setSettingsActive: createSetState("isSettingsActive"),
-    setNumberOfLetter: (state, action: PayloadAction<number>) => {
-      resetGame();
-      return restartGameAction(state, state.words, action.payload);
-    },
   },
 });
 
@@ -94,24 +42,24 @@ export const {
   setNumberOfLetter,
 } = appSlice.actions;
 
-export const letterSelector = ({ currentRow, word, words, numberOfLetters }: AppState) => ({
+export const letterSelector = ({ currentRow, word, words, numberOfLetters }: RootState) => ({
   currentRow,
   word,
   words,
   numberOfLetters,
 });
 
-export const panelSelector = ({ gameIs, keys, modal }: AppState) => ({ gameIs, keys, modal });
+export const panelSelector = ({ gameIs, keys, modal }: RootState) => ({ gameIs, keys, modal });
 
-export const stateSelector = ({ gameIs, word }: AppState) => ({ gameIs, word });
+export const stateSelector = ({ gameIs, word }: RootState) => ({ gameIs, word });
 
-export const gameSelector = ({ backspace, enter }: AppState) => ({ backspace, enter });
+export const gameSelector = ({ backspace, enter }: RootState) => ({ backspace, enter });
 
-export const gameHookSelector = ({ backspace, currentRow, enter, gameIs, keys, words, numberOfLetters }: AppState) => {
+export const gameHookSelector = ({ backspace, currentRow, enter, gameIs, keys, words, numberOfLetters }: RootState) => {
   return { backspace, currentRow, enter, keys, isFinished: gameIs !== "playing", words, numberOfLetters };
 };
 
-export const settingsSelector = ({ isSettingsActive, numberOfLetters }: AppState) => ({
+export const settingsSelector = ({ isSettingsActive, numberOfLetters }: RootState) => ({
   isSettingsActive,
   numberOfLetters,
 });
---
+++ utils/store.ts
@@ -0,0 +1,62 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+import { encode } from "js-base64";
+import { DEFAULT_NUMBER_OF_LETTERS } from "./numbers-of-letters";
+
+export interface RootState {
+  isSettingsActive: boolean;
+  numberOfLetters: number;
+  backspace: boolean;
+  currentRow: number;
+  enter: boolean;
+  gameIs: "playing" | "won" | "lost";
+  keys: Record<number, string[]>;
+  modal: { content: string; isOpen: boolean; showButton: boolean };
+  word: string;
+  words: string[];
+}
+
+export const createInitialState = (): RootState => ({
+  isSettingsActive: false,
+  numberOfLetters: DEFAULT_NUMBER_OF_LETTERS,
+  backspace: false,
+  currentRow: 0,
+  enter: false,
+  gameIs: "playing",
+  keys: { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [] },
+  modal: { isOpen: false, content: "", showButton: false },
+  word: "",
+  words: [],
+});
+
+export const createSetState = <T extends keyof RootState>(property: T, mergeObj?: boolean) => {
+  return (
+    state: RootState,
+    action: PayloadAction<RootState[T] extends Record<string, unknown> ? Partial<RootState[T]> : RootState[T]>
+  ) => {
+    if (!mergeObj) {
+      state[property] = action.payload as any;
+      return state;
+    }
+
+    const obj = state[property];
+    if (typeof obj === "object" && typeof action.payload === "object") state[property] = { ...obj, ...action.payload };
+  };
+};
+
+export const getRandomWord = (words: string[]) => encode(words[Math.floor(Math.random() * words.length)]);
+
+export const getWords = (words: string[], length: number) => {
+  return words.filter((word): word is string => typeof word === "string" && word.length === length);
+};
+
+export const restartGameAction = (state: RootState, words?: string[], numberOfLetters?: number): RootState => {
+  const wordsWithNumberOfLetters = getWords(words ?? state.words, numberOfLetters ?? state.numberOfLetters);
+
+  return {
+    ...createInitialState(),
+    numberOfLetters: numberOfLetters ?? state.numberOfLetters,
+    words: words ?? state.words,
+    word: getRandomWord(wordsWithNumberOfLetters),
+    gameIs: "playing",
+  };
+};
