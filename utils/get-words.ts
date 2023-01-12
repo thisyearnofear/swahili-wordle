10055c6e2c06fab4975d87cdd3656a46d04b6794
iswilljr <iswilljr@gmail.com>
2023-01-12 16:18:46 -0500

    fix: filter words after fetching

--- utils/get-words.ts
+++ utils/get-words.ts
@@ -1,18 +1,7 @@
-interface Options {
-  length?: number;
-  filter?: boolean;
-}
-
-export async function getWords(options: Options = {}): Promise<string[]> {
-  const { filter = true, length = 5 } = options;
-
+export async function getWords(): Promise<string[]> {
   const wordsJson = await fetch("/words.json").then((res) => (res.ok ? res.json() : Promise.reject(res)));
 
   if (!Array.isArray(wordsJson)) throw Error("Invalid data");
 
-  if (!filter) return wordsJson;
-
-  const words = wordsJson.filter((word): word is string => typeof word === "string" && word.length === length);
-
-  return words;
+  return wordsJson;
 }
