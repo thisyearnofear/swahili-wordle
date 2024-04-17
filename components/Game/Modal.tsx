6e41c949bee6cb9f7fa67afeb38cf60be1ada148
iswilljr <iswilljr@gmail.com>
2024-04-17 15:50:55 -0500

    improve modal component design

--- components/Game/Modal.tsx
+++ components/Game/Modal.tsx
@@ -1,6 +1,7 @@
 import { IconX } from "@tabler/icons-react";
+import { useOnClickOutside } from "usehooks-ts";
 import { useTranslation } from "hooks/use-translations";
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 
 interface ModalProps {
   title: string;
@@ -14,23 +15,25 @@ interface ModalProps {
 export function Modal({ active, className, children, title, titleClass, onClose }: ModalProps) {
   const translation = useTranslation();
   const modalRef = useRef<HTMLDivElement>(null);
+  const modalOverlayRef = useRef<HTMLDivElement>(null);
+
+  const handleClose = useCallback(() => {
+    modalRef.current?.classList.remove("active");
+    modalOverlayRef.current?.classList.remove("active");
+    onClose?.();
+  }, [onClose]);
+
+  useOnClickOutside(modalRef, handleClose);
 
   return (
-    <div ref={modalRef} className={`modal_finish${active ? " active" : ""} ${className ?? ""}`}>
-      <div className={`top ${titleClass}`}>{title}</div>
-      <div className="data">{children}</div>
-      <button
-        type="button"
-        className="close"
-        aria-label={translation.close}
-        onClick={() => {
-          if (!modalRef.current) return;
-          modalRef.current.classList.remove("active");
-          onClose?.();
-        }}
-      >
-        <IconX />
-      </button>
+    <div ref={modalOverlayRef} className={`modal_overlay ${active ? " active" : ""}`}>
+      <div ref={modalRef} className={`modal_finish${active ? " active" : ""} ${className ?? ""}`}>
+        <div className={`top ${titleClass}`}>{title}</div>
+        <div className="data">{children}</div>
+        <button type="button" className="close" aria-label={translation.close} onClick={handleClose}>
+          <IconX />
+        </button>
+      </div>
     </div>
   );
 }
